a175d3ee8b4846f5545c1ab1a4d04e9b0bf07b96
Yujung <ujungg3@gmail.com>
2024-09-11 00:45:17 +0900

    scrysort

--- 17week/ScrySort/q.cpp
+++ 17week/ScrySort/q.cpp
@@ -5,7 +5,24 @@
 //TODO:
 void Scry2Sort(const std::vector<int>& target, deck& obj)
 {
-    
+	int size = target.size();
+	int flag = target[0];
+	while (flag != obj.Top())
+	{
+		for (int i = 0; i < size; i++)
+		{
+			while (target[i] != obj.Top())
+			{
+				if (target[i] == obj.Other())
+					obj.Swap();
+				else
+					obj.Pushback();
+			}
+
+			if (target[i] == obj.Top())
+				obj.Pushback();
+		}
+	}
 }
 
 
